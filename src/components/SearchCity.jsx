133627a230792fa2bd88241c50350b73374475cc
Ayo89 <ayose89@gmail.com>
2024-10-07 01:46:49 +0530

    Add handleCity and getWeatherData

--- src/components/SearchCity.jsx
+++ src/components/SearchCity.jsx
@@ -1,25 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import fetchWeatherData from "../services/weather";
 
 function SearchCity() {
-  function handleSearchCity (event) {
-    console.log(event)
+  const [city, setCity] = useState("");
+  function handleCity(event) {
+    setCity(event.target.value);
   }
+
+  const getWeatherData = async (city) => {
+    const weatherData = await fetchWeatherData(city);
+    console.log(weatherData);
+  };
 
   return (
     <section className="bg-blue-300 h-20 ">
       <form
         className="h-full flex flex-row items-center justify-around"
         action="submit"
+        onSubmit={(e) => {
+          e.preventDefault();
+          getWeatherData(city);
+        }}
       >
         <input
           className="pl-6 py-2 rounded-lg"
           type="text"
           placeholder="ENTER A CITY"
+          onChange={handleCity}
         />
-        <button
-          className="w-56 bg-customGreen rounded-lg p-1 text-white "
-          onClick={handleSearchCity}
-        >
+        <button className="w-56 bg-customGreen rounded-lg p-1 text-white ">
           CHECK WEATHER &rarr;
         </button>
       </form>
